c5a2a62e3e3c576f78f20b6e18edb5c5126dc8dc
DAH 14 <dah14@FORMATION.lan>
2020-02-07 11:32:28 +0100

    premier commit

---
+++ precache-manifest.3359a5690bc9116cd0d7f9a5b62de4e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2ed0dc4e006776e254fa3f7b32a20d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3df483852e2175c289f6",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b4154f88674d77bfbf55",
+    "url": "/static/js/2.b6f21eb3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.b6f21eb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3df483852e2175c289f6",
+    "url": "/static/js/main.9f86ada6.chunk.js"
+  },
+  {
+    "revision": "a00d9fd76856252b14b7",
+    "url": "/static/js/runtime-main.7aa1488f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
